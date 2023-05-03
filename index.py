b461e8fe6b74755c6eae9c65e98eb6da1b424996
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-03 08:26:14 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,10 @@
+#Replace any negative array values with 'Dojo'
+
+def replace_negatives(list):
+
+    for i in range(0,len(list)):
+        if (list[i] < 0):
+            list[i] = 'Dojo'
+    return list
+
+print(replace_negatives([2,4,-12,8,-2]))
